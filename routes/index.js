4c3f7d5f1b1d49c6ce072d8a85d8c48fe15e6846
Tomás Antunes <tomasantunes@gmail.com>
2026-03-16 19:17:03 +0000

    Initial commit.

---
+++ routes/index.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var router = express.Router();
+var database = require('../libs/database');
+var secretConfig = require('../secret-config');
+
+var { con, con2 } = database.getMySQLConnections();
+
+router.get('/login', (req, res) => {
+  var token = req.query.token;
+  if (token === secretConfig.LOGIN_TOKEN) {
+    req.session.isLoggedIn = true;
+    res.redirect('/');
+  } else {
+    res.status(401).send('Invalid Authorization');
+  }
+});
+
+router.get('/', function(req, res, next) {
+  if (!req.session.isLoggedIn) {
+    return res.status(401).send('Invalid Authorization');
+  }
+  res.render('index');
+});
+
+router.post('/add-expense', async (req, res) => {
+  if (!req.session.isLoggedIn) {
+    return res.render('add-expense', { message: 'Invalid Authorization' });
+  }
+
+  const { amount, category_id, category_name } = req.body;
+
+  if (!amount) {
+    return res.render('add-expense', { message: 'Amount is required' });
+  }
+
+  if (!category_id && !category_name) {
+    return res.render('add-expense', { message: 'Category is required' });
+  }
+
+  if (category_id === 'new' && category_name != '') {
+    const query = 'INSERT INTO expense_categories (name) VALUES (?)';
+    var res = await con2.execute(query, [category_name]);
+    category_id = res[0].insertId;
+  }
+
+  const query = 'INSERT INTO expense_tracker (amount, category_id) VALUES (?, ?)';
+  con.query(query, [amount, category_id], (err, result) => {
+    if (err) {
+      console.log('Error inserting expense:', err);
+      return res.render('add-expense', { message: 'Error adding expense to database.' });
+    }
+
+    res.render('add-expense', { message: 'Expense added successfully!' });
+  });
+});
+
+router.get('/get-categories', (req, res) => {
+  if (!req.session.isLoggedIn) {
+    return res.json({ status: "NOK", error: 'Invalid Authorization' });
+  }
+
+  const query = 'SELECT id, name FROM category';
+
+  con.query(query, (err, result) => {
+    if (err) {
+      console.log('Error fetching categories:', err);
+      return res.json({ status: "NOK", error: 'Error fetching categories from database.' });
+    }
+
+    res.json({ status: "OK", data: result });
+  });
+});
+module.exports = router;
